1efb3a35b966c16718ea64adb0657aacfa9a6e02
silvia <silviauzie77@gmail.com>
2025-05-19 08:43:55 +0100

    commit one

---
+++ practice.js
@@ -0,0 +1,27 @@
+/*let name = "John Doe";
+let age = 30;
+let selectedColors = ["red", "blue"];
+selectedColors[2] = "green"; // Adding a new color to the array
+console.log(selectedColors);
+function greet() {
+    console.log("Hello, " + name + "!");
+}
+greet('mary'); // Calling the greet function
+greet('silvia');
+document.getElementById("main").style.display = "none"; // Hiding the main element
+alert("Hello"); // Displaying an alert with the name
+console.log('Hello' + 'silvia'); // Logging a greeting message to the console
+console.log(2+2)*/
+
+console.log('Hello' +" "+ 'Silvia');
+let Age = 25;
+let Developer = "Silvia";
+console.log(Developer);
+console.log(Developer + Age);
+let Result=(Age + Developer);
+//reassigning a value to the variable
+ Age = 18;
+console.log(Age);
+Age = Age+5;
+console.log(Age);
+console.log(typeof Age);
